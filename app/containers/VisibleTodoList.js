be1892b3fa0c8f8ec8f00b8c7760dca16dc1a174
Michael Aranda <michaelaranda0.web@gmail.com>
2016-12-28 15:59:35 -0800

    Finishes implementation of basic React Redux Todo list official example

---
+++ app/containers/VisibleTodoList.js
@@ -0,0 +1,45 @@
+import { connect } from 'react-redux'
+import { toggleTodo } from '../actions'
+import TodoList from '../components/TodoList'
+
+const getVisibleTodos = (todos, filter) => {
+  switch(filter) {
+    case 'SHOW_ALL':
+      return todos;
+    case 'SHOW_COMPLETED':
+      return todos.filter(t => t.completed);
+    case 'SHOW_ACTIVE':
+      return todos.filter(t => !t.completed);
+    default:
+      return todos;
+  }
+};
+
+const mapStateToProps = (state) => {
+  return {
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+  }
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: (id) => {
+      dispatch(toggleTodo(id))
+    }
+  }
+}
+
+const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList);
+
+export default VisibleTodoList
+
+//TODO: Understand how to convert above to match below instead
+
+// export default connect(state => {
+//   return {
+//     subscriptions: state.subscriptions
+//   };
+// }, {fetchSubscription, fetchNotifications})(App);
--- app/index.js
+++ app/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import todoApp from './reducers';
 import App from './components/App';
 import './stylesheets/index.css';
 
+let store = createStore(todoApp);
+
+store.dispatch({
+  id: 1,
+  type: 'ADD_TODO',
+  completed: true,
+  text: 'Example to-do list item'
+});
+
+store.dispatch({
+  id: 2,
+  type: 'ADD_TODO',
+  completed: false,
+  text: 'Another example'
+});
+
 ReactDOM.render(
-  <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
   document.getElementById('root')
 );
